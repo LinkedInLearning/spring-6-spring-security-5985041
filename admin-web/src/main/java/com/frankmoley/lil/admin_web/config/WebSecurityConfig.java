98c99fcf2e1ee2c1d78615dbd3079522e46ab4d5
Frank P Moley III <frankmoley@gmail.com>
2025-03-30 22:01:07 +0000

    adding files for 02_05

--- admin-web/src/main/java/com/frankmoley/lil/admin_web/config/WebSecurityConfig.java
+++ admin-web/src/main/java/com/frankmoley/lil/admin_web/config/WebSecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,10 +22,19 @@ public class WebSecurityConfig {
     http
       .authorizeHttpRequests((requests)-> requests
         .requestMatchers("/", "/index").permitAll()
+        .requestMatchers("/customers/**").hasRole("USER")
+        .requestMatchers("/orders").hasRole("ADMIN")
         .anyRequest().authenticated()
       )
       .httpBasic(Customizer.withDefaults());
       return http.build();
+  }
+
+  @Bean
+  public GrantedAuthoritiesMapper authoritiesMapper(){
+    SimpleAuthorityMapper authorityMapper = new SimpleAuthorityMapper();
+    authorityMapper.setConvertToUpperCase(true);
+    return authorityMapper;
   }
 
   @Bean
