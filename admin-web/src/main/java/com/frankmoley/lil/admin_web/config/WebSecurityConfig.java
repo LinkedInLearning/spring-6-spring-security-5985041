ef4b9dfd4244c48414b8d167e38fdde54122fa33
Frank P Moley III <frankmoley@gmail.com>
2025-03-30 03:49:13 +0000

    adding files for 02_03

--- admin-web/src/main/java/com/frankmoley/lil/admin_web/config/WebSecurityConfig.java
+++ admin-web/src/main/java/com/frankmoley/lil/admin_web/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.frankmoley.lil.admin_web.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,7 +10,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -27,12 +32,13 @@ public class WebSecurityConfig {
   }
 
   @Bean
-  public UserDetailsService userDetailsService(){
-    UserDetails user = User.withDefaultPasswordEncoder()
-      .username("user")
-      .password("password")
-      .roles("USER")
-      .build();
-    return new InMemoryUserDetailsManager(user);
+  public UserDetailsService userDetailsService(DataSource dataSource){
+    return new JdbcUserDetailsManager(dataSource);
   }
+
+  @Bean
+  public static PasswordEncoder getPasswordEncoder(){
+    return NoOpPasswordEncoder.getInstance();
+  }
+
 }
